41523aa63f12db06f2d7fe46dc5ed1495275ffff
AusKruger <firdausam98@gmail.com>
2023-10-31 23:03:59 +0800

    commit-20231031

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hello! Welcome to Contoso App Application");
+
+string[] ourAnimals = new string[] { "786", "Dog", "4 years", "Cute", "69", "Cat", "2 years", "Nimble" };
+
+
+//foreach(string animal in ourAnimals)
+//{
+//    Console.WriteLine(animal);
+//}
+
+bool continueUsing = true;
+
+while (continueUsing) { 
+Console.WriteLine("\nNo.\tOptions");
+Console.WriteLine("1.\tList Available your animal");
+Console.WriteLine("2.\tAdd new animal");
+
+
+
+}
